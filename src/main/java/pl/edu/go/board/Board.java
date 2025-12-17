12fa8921a17dbaf3ea403854c12314658e167b61
Piotr <piotrkorpus299@example.com>
2025-12-17 02:15:32 +0100

    Dodanie szczeg komentarzy

--- src/main/java/pl/edu/go/board/Board.java
+++ src/main/java/pl/edu/go/board/Board.java
@@ -120,7 +120,7 @@ public class Board {
         board[x][y] = color;
 
         int opp = (color == BLACK ? WHITE : BLACK);
-        boolean captured = false;
+        boolean captured = false; // zmienna pozwalająca określić czy dany rych jest legalny
 
         for (int[] nb : neighbors(x, y)) {
             int nx = nb[0], ny = nb[1];
